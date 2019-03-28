162cd1a046e44c316724f9d614f870ade60b1ede
zzl <zzl@Eylaine.local>
2019-03-28 15:07:45 +0800

    add json compare util

--- src/main/java/utils/JsonUtil.java
+++ src/main/java/utils/JsonUtil.java
@@ -2,10 +2,14 @@ package utils;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.google.gson.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Description: 解析Json字符串，返回Map
@@ -16,6 +20,9 @@ import java.util.ArrayList;
 public class JsonUtil {
 
     private static Logger LOGGER = LoggerFactory.getLogger(JsonUtil.class);
+
+    private static Gson gson = new Gson();
+    private static JsonParser jsonParser = new JsonParser();
 
     /**
      * 从Json串中读取value，目前只支持三层json嵌套
@@ -85,6 +92,133 @@ public class JsonUtil {
         }
 
         return al;
+    }
+
+    /**
+     * 比较两个bean是否等价
+     * @param oa
+     * @param ob
+     * @return
+     */
+    public static boolean compare(Object oa, Object ob) {
+
+        if (null == oa) {
+            return ob == null;
+        }
+
+        return compare(gson.toJson(oa), gson.toJson(ob));
+    }
+
+    /**
+     * 比较json字符串
+     * @param sa
+     * @param sb
+     * @return
+     */
+    public static boolean compare(String sa, String sb) {
+
+        if (null == sa) {
+            return null == sb;
+        }
+
+        if (sa.equals(sb)) {
+            return true;
+        }
+
+        JsonElement ja = jsonParser.parse(sa);
+        JsonElement jb = jsonParser.parse(sb);
+
+        if (gson.toJson(ja).equals(gson.toJson(jb))) {
+            return true;
+        }
+
+        return compare(ja, jb);
+    }
+
+    /**
+     * 比较json数组
+     * @param aa
+     * @param ab
+     * @return
+     */
+    private static boolean compare(JsonArray aa, JsonArray ab) {
+
+        if (aa.size() != ab.size()) {
+            return false;
+        }
+
+        List<JsonElement> la = sortList(aa);
+        List<JsonElement> lb = sortList(ab);
+
+        for (int i = 0; i < la.size(); i++) {
+            if (!compare(la.get(i), lb.get(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * json数组排序
+     * @param ja
+     * @return
+     */
+    private static List<JsonElement> sortList(JsonArray ja) {
+        List<JsonElement> jlist = new ArrayList<>();
+        ja.forEach(jlist::add);
+        jlist.sort(Comparator.comparing(gson::toJson));
+        return jlist;
+    }
+
+    /**
+     * 比较Json对象
+     * @param joa
+     * @param job
+     * @return
+     */
+    private static boolean compare(JsonObject joa, JsonObject job) {
+        Set<String> aSet = joa.keySet();
+        Set<String> bSet = job.keySet();
+
+        if (!aSet.equals(bSet)) {
+            return false;
+        }
+
+        for (String aKey: aSet) {
+            if (!compare(joa.get(aKey), job.get(aKey))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     *
+     * @param pa
+     * @param pb
+     * @return
+     */
+    private static boolean compare(JsonPrimitive pa, JsonPrimitive pb) {
+        return pa.equals(pb);
+    }
+
+    /**
+     *
+     * @param ea
+     * @param eb
+     * @return
+     */
+    private static boolean compare(JsonElement ea, JsonElement eb) {
+
+        if (ea.isJsonObject() && eb.isJsonObject()) {
+            return compare((JsonObject) ea, (JsonObject)eb);
+        } else if (ea.isJsonArray() && eb.isJsonArray()) {
+            return compare((JsonArray)ea, (JsonArray)eb);
+        } else if (ea.isJsonPrimitive() && eb.isJsonPrimitive()) {
+            return compare((JsonPrimitive)ea, (JsonPrimitive)eb);
+        } else return ea.isJsonNull() && eb.isJsonNull();
     }
 
 }
